aea72b0a919bb496126d029fb3eea8cf376f1a18
eachen <stopratracingplz@gmail.com>
2025-10-10 20:17:56 +0800

    update

--- shell_scripts/pde_burgers_tfno_dim1.sh
+++ shell_scripts/pde_burgers_tfno_dim1.sh
@@ -1,6 +1,6 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=4
 
-python -m scripts.train PDEBench FNO \
+python -m scripts.train_dim PDEBench FNO \
     --data_folder /data/ycsong/pdebench/1D/Burgers/Train \
     --task burgers \
     --n_train 900 \
@@ -12,7 +12,7 @@ python -m scripts.train PDEBench FNO \
     --batch_size 128 \
     --train_subsample_rate 2 \
     --test_subsample_rate 2 \
-    --time_step 1 \2
+    --time_step 1 \
     --time_skips 10 \
     --n_modes 21 \
     --channel_mixing mlp \
@@ -25,11 +25,18 @@ python -m scripts.train PDEBench FNO \
     --projection_channels 64 \
     --factorization tucker \
     --rank 0.42 \
+    --norm dim_norm1 \
+    --pre_norm 1 \
+    --preactivation 1 \
+    --prediction_dims 0 \
+    --num_consts 1 \
+    --append_dimless 0 \
+    --pos_aug_consts 0 \
     --lr 1e-3 \
     --weight_decay 1e-4 \
     --scheduler_steps 100 \
     --scheduler_gamma 0.5 \
-    --train_loss l2 \
+    --train_loss h1 \
     --epochs 1001 \
     --verbose 1 \
     --random_seed 0 \
